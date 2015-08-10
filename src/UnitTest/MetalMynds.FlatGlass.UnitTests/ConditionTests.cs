f943063ad9a63535849d142597e362699c825262
Dave Paterson <crazeydave.2.0@gmail.com>
2015-08-10 21:35:20 +0100

    Updated: Unit Tests
    Warning! Unit Tests Build but fail!
    
    Added a copy of the WiPFlash library (no nuget package).
    Added Binaries for examples of UI Both Wpf/WinForms.

--- src/UnitTest/MetalMynds.FlatGlass.UnitTests/ConditionTests.cs
+++ src/UnitTest/MetalMynds.FlatGlass.UnitTests/ConditionTests.cs
@@ -1,70 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-using Antlr4;
-using Antlr4.Runtime;
-using Antlr4.Runtime.Tree;
-//using Antlr4.Runtime.Debug;
+//using Antlr4;
+//using Antlr4.Runtime;
+//using Antlr4.Runtime.Tree;
+////using Antlr4.Runtime.Debug;
 
-using MetalMynds.FlatGlass;
+//using MetalMynds.FlatGlass;
 
-using EntMapping;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using EntMapping;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace MetalMynds.FlatGlass.UnitTests
-{
-    [TestClass]
-    public class ConditionTests
-    {
-        [TestMethod]
-        public void GenerateAst()
-        {
-            //ANTLRStringStream expression = new ANTLRStringStream("john =='dave' AND (joe OR sue)");
-            ANTLRStringStream expression = new ANTLRStringStream("john AND (joe OR sue)");
-            var tokens = new CommonTokenStream(new ConditionLexer(expression));
-            var parser = new ConditionParser(tokens);
+//namespace MetalMynds.FlatGlass.UnitTests
+//{
+//    [TestClass]
+//    public class ConditionTests
+//    {
+//        [TestMethod]
+//        public void GenerateAst()
+//        {
+//            //ANTLRStringStream expression = new ANTLRStringStream("john =='dave' AND (joe OR sue)");
+//            ANTLRStringStream expression = new ANTLRStringStream("john AND (joe OR sue)");
+//            var tokens = new CommonTokenStream(new ConditionLexer(expression));
+//            var parser = new ConditionParser(tokens);
 
-            var adaptor = new CommonTreeAdaptor();
+//            var adaptor = new CommonTreeAdaptor();
 
-            parser.TreeAdaptor = adaptor;
+//            parser.TreeAdaptor = adaptor;
 
-            var result = parser.prog();
+//            var result = parser.prog();
 
-            var tree = (CommonTree)result.Tree;
+//            var tree = (CommonTree)result.Tree;
 
-            Print(tree, 0);
+//            Print(tree, 0);
 
-        }
+//        }
 
-        [TestMethod]
-        public void GenerateBetterAst()
-        {
-            //ANTLRStringStream expression = new ANTLRStringStream("john =='dave' AND (joe OR sue)");
-            ANTLRStringStream expression = new ANTLRStringStream("Invoice <- Order ;\n\rInvoiceDate = Order.OrderDate + 30 ;\n\rInvoiceNo = GetNextInvoiceNo() ;\n\rFreight = Order.TotalCBM * 1.5 + Order.TotalWeight * 2.2;\n\rShipVia = IF(Order.IsLocal, \"Express\", \"Mail\") ;");
-            var tokens = new CommonTokenStream(new ConditionLexer(expression));
-            var parser = new EntityMappingParser(tokens);
+//        [TestMethod]
+//        public void GenerateBetterAst()
+//        {
+//            //ANTLRStringStream expression = new ANTLRStringStream("john =='dave' AND (joe OR sue)");
+//            ANTLRStringStream expression = new ANTLRStringStream("Invoice <- Order ;\n\rInvoiceDate = Order.OrderDate + 30 ;\n\rInvoiceNo = GetNextInvoiceNo() ;\n\rFreight = Order.TotalCBM * 1.5 + Order.TotalWeight * 2.2;\n\rShipVia = IF(Order.IsLocal, \"Express\", \"Mail\") ;");
+//            var tokens = new CommonTokenStream(new ConditionLexer(expression));
+//            var parser = new EntityMappingParser(tokens);
 
-            var adaptor = new CommonTreeAdaptor();
+//            var adaptor = new CommonTreeAdaptor();
 
-            parser.TreeAdaptor = adaptor;
+//            parser.TreeAdaptor = adaptor;
 
-            var result = parser.prog();
+//            var result = parser.prog();
 
-            var tree = (CommonTree)result.Tree;
+//            var tree = (CommonTree)result.Tree;
 
-            Print(tree, 0);
+//            Print(tree, 0);
 
-        }
+//        }
 
-        static void Print(CommonTree tree, int level)
-        {
-            System.Diagnostics.Debug.WriteLine(new string('\t', level) + tree.Text);
-            if (tree.Children != null)
-                foreach (CommonTree child in tree.Children)
-                    Print(child, level + 1);
-        }
+//        static void Print(CommonTree tree, int level)
+//        {
+//            System.Diagnostics.Debug.WriteLine(new string('\t', level) + tree.Text);
+//            if (tree.Children != null)
+//                foreach (CommonTree child in tree.Children)
+//                    Print(child, level + 1);
+//        }
 
-    }
+//    }
 
-}
+//}
---
+++ src/UnitTest/MetalMynds.FlatGlass.UnitTests/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WiPFlash;
+using WiPFlash.Components;
+
+namespace MetalMynds.FlatGlass.UnitTests
+{
+    public static class Extensions
+    {
+        public static Boolean EnsureVisible(this AutomationElementWrapper element)
+        {
+            return false;
+        }
+    }
+}
