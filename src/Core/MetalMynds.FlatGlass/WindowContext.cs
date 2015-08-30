f68ca590738ffbe32b7f5e5244d16695f71db429
Dave Paterson <crazeydave.2.0@gmail.com>
2015-08-30 19:03:07 +0100

    Updated: Dependencies for Prevail
          Jar and Run Times for Antlr4.

---
+++ src/Core/MetalMynds.FlatGlass/WindowContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MetalMynds.FlatGlass
+{
+    public class WindowContext
+    {
+        private readonly List<IHoldPlace> _placeHolders;
+
+        private WindowContext()
+        {
+            _placeHolders = new List<IHoldPlace>();
+        }
+
+        public List<IHoldPlace> PlaceHolders { get { return _placeHolders; } }
+
+        public virtual void Close()
+        {
+            _placeHolders.Clear();
+        }
+
+        public static WindowContext Create()
+        {
+            return new WindowContext();
+        }
+    }
+}
