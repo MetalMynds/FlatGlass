c7701eec3c66434486f1bea2e798166448a890cf
Dave Paterson <crazeydave.2.0@gmail.com>
2015-09-06 17:37:45 +0100

    Updated: PrevailListener & Unit Tests
            Fixed Defect in And and Or Conditions caused by pop of stack in wrong order.

--- src/UnitTest/MetalMynds.FlatGlass.UnitTests/ConditionTests.cs
+++ src/UnitTest/MetalMynds.FlatGlass.UnitTests/ConditionTests.cs
@@ -50,10 +50,19 @@ namespace MetalMynds.FlatGlass.UnitTests
                     new PropertyCondition(AutomationElement.ClassNameProperty, "Awsum"), 
                     new PropertyCondition(AutomationElement.NameProperty, "Adi"));
 
+
+            var expression3 = new ExpressionLocator("", 1, Scope.Descendants, "Class = 'Awsum' And Name = 'Adi'");
+
+            var result3Expression = expression3.Condition;
+
+            Assert.IsTrue(AutomationHelper.Compare(result3, result3Expression));
+
+        
+        
         }
 
 
 
     }
 
-}
+} 
